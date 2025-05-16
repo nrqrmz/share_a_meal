c011ca06a0015f149ce29d9cfd72e95e87a0a010
Enrique Ramirez <nrqrmz@gmail.com>
2025-05-16 01:19:15 -0600

    add geolocation feature

--- app/javascript/controllers/map_index_controller.js
+++ app/javascript/controllers/map_index_controller.js
@@ -3,15 +3,29 @@ import mapboxgl from 'mapbox-gl'
 
 export default class extends Controller {
   static values = { apiKey: String }
+  static targets = ["map"]
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
-      container: this.element,
+      container: this.mapTarget,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [-73.5673, 45.5019],
       zoom: 13
     })
+
+    this.map.addControl(new mapboxgl.NavigationControl(), 'top-left')
+  }
+
+  locatePosition() {
+    navigator.geolocation.getCurrentPosition(position => {
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+    this.map.flyTo({ center: [lng, lat], zoom: 14 });
+    new mapboxgl.Marker({ color: "#d00" })
+      .setLngLat([lng, lat])
+      .addTo(this.map);
+  })
   }
 }
