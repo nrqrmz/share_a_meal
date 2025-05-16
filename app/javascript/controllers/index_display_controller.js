50940bd9f73e069f1c3630bb8c747b12773c0ec1
Enrique Ramirez <nrqrmz@gmail.com>
2025-05-16 01:52:46 -0600

    add display map / list behaviour

---
+++ app/javascript/controllers/index_display_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="index-display"
+export default class extends Controller {
+  static targets = [
+    "listButton",
+    "mapButton",
+    "mealList",
+    "mealMap"
+  ]
+
+  connect() {
+    console.log('display connected');
+  }
+
+  displayMealList() {
+    this.listButtonTarget.classList.add('active')
+    this.mapButtonTarget.classList.remove('active')
+    this.mealListTarget.style.display = 'block'
+    this.mealMapTarget.style.display = 'none'
+  }
+
+  displayMealMap() {
+    this.mapButtonTarget.classList.add('active')
+    this.listButtonTarget.classList.remove('active')
+    this.mealListTarget.style.display = 'none'
+    this.mealMapTarget.style.display = 'block'
+  }
+}
